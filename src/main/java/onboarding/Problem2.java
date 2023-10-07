c5d552abb78780b23707a525e03741390937091c
maark1106 <mark1106@naver.com>
2023-10-07 12:31:35 +0900

    Problem2 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        System.out.println("hello");
         String answer = "answer";
         return answer;
     }
