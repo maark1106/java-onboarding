829e973501c60c5b66237e48f6dc540a0127e300
maark1106 <mark1106@naver.com>
2023-10-07 17:08:25 +0900

    Problem2 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
+
+    static Code code = new Code();
     public static String solution(String cryptogram) {
-        System.out.println("hello");
-        String answer = "answer";
-        return answer;
+        return code.decode(cryptogram);
+    }
+
+    static class Code{
+        public String decode(String cryptogram){
+            Stack<Character> stack = new Stack<>();
+
+            int index = 0;
+            while(index < cryptogram.length()){
+                if(stack.isEmpty() || !stack.peek().equals(cryptogram.charAt(index))){
+                    stack.push(cryptogram.charAt(index));
+                    ++index;
+                    continue;
+                }
+                while(index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index))){
+                    ++index;
+                }
+                stack.pop();
+            }
+
+            return stack.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining());
+        }
     }
 }
